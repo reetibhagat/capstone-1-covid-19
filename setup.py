086f1c507e60f9bf49704214b99a5721c0b41033
Reeti Bhagat <bhagat.reeti@gmail.com>
2020-06-27 13:29:39 -0700

    Cookiecutter initial project files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PreForecasting for covid-19 and analysis.',
+    author='Reeti Bhagat',
+    license='MIT',
+)
